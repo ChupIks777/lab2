75e79a3a1662ac28bff1439b637dca3c7bdf64ef
ChupIks777 <145923594+ChupIks777@users.noreply.github.com>
2024-11-17 11:49:16 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,13 @@ import ru.ifmo.se.pokemon.*;
 public class Main {
     public static void main(String[] args) {
         Battle battle = new Battle();
+        
         Pokemon diancie = new Diancie("Дайанси", 10);
         Pokemon koffing = new Koffing("Коффинг", 17);
         Pokemon weezing = new Weezing("Визинг", 25);
         Pokemon porygon = new Porygon("Поригон", 38);
         Pokemon porygon2 = new Porygon2("Поригон2", 21);
-        Pokemon PorygonZ = new PorygonZ("Поригон-Зед", 11);
+        Pokemon porygonZ = new PorygonZ("Поригон-Зед", 11);
         
         battle.addAlly(diancie);
         battle.addAlly(weezing);
